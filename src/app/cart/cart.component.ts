a067184d6abf41598fb3a95989964d73b77b2367
ChalakaK94 <karunarathne.cp@gmail.com>
2023-12-11 17:56:21 +0530

    cart component and service initial commit

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../services/cart/cart.service";
+import {Cart} from "../shared/models/Cart";
+import {CartItem} from "../shared/models/CartItem";
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit{
+
+  cart!:Cart
+  constructor(private cartService:CartService) {
+    this.setCart()
+  }
+
+  ngOnInit(): void {
+  }
+  setCart(){
+    this.cart = this.cartService.getCart()
+  }
+
+  removeFromCart(cartItem:CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: HTMLSelectElement){
+    const quantity = parseInt(quantityInString.value);
+
+    this.cartService.changeQuantity(cartItem.food.id, quantity)
+    this.setCart()
+  }
+
+}
